7e54119154c87c51b23f424c4dfb8fbf69dfbe88
HYnam <namhy2006@gmail.com>
2021-05-05 19:00:42 +0800

    initialize AbstractGrid

--- task1.py
+++ task1.py
@@ -6,3 +6,13 @@ import PIL
 
 from a2_solution import advanced_game
 from constants import TASK, MAP_FILE
+
+class AbstractGrid(tk.Canvas):
+    """
+    provides base functionality for other view classes. An AbstractGrid can be thought of as a grid.
+    """
+    def __init__(self, master, rows, cols, width, height, **kwargs):
+        self.rows = rows
+        self.cols = cols
+        self.width = width
+        self.height = height
