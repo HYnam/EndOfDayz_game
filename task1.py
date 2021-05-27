07b3f499752269705d08bbc479ad6369c223fc6e
HYnam <namhy2006@gmail.com>
2021-05-27 21:45:23 +0800

    Initialize AbstractGrid class

--- task1.py
+++ task1.py
@@ -1,21 +1,23 @@
 import tkinter as tk
-import typing
-import math
-import tkinter.messagebox
-import PIL
+import tkinter.messagebox as messagebox
 
-from a2_solution import advanced_game
-from constants import TASK, MAP_FILE
+import a2_solution as a2
+from constants import *
 
 class AbstractGrid(tk.Canvas):
     """
     provides base functionality for other view classes. An AbstractGrid can be thought of as a grid.
     """
     def __init__(self, master, rows, cols, width, height, **kwargs):
-        self.rows = rows
-        self.cols = cols
-        self.width = width
-        self.height = height
+        self._master = master
+        self._rows = rows
+        self._cols = cols
+        self._width = width
+        self._height = height
+        self._cell_width = self._width // self._cols
+        self._cell_height = self._height // self._rows
+        super().__init__(master, **kwargs)
+        self.config(width=width, height=height)
 
     def get_bbox(self, position):
         """
