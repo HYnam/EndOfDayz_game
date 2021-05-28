b23f15c5aa3af2937eb73846b9d1ec6acabb9741
HYnam <namhy2006@gmail.com>
2021-05-28 11:50:30 +0800

    a3

--- a3.py
+++ a3.py
@@ -3,7 +3,6 @@ A GUI-based zombie survival game wherein the player has to reach
 the hospital whilst evading zombies.
 """
 
-# Replace these <strings> with your name, student number and email address.
 __author__ = "Nam Hiu Yi, 46604563"
 __email__ = "hiuyi.nam@uqconnect.edu.au"
 
